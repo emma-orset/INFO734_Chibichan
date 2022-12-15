1ed1279aebfb4db9ef205ce411311a1fbd491a0d
Emma ORSET <eiko.kamui@gmail.com>
2022-12-15 17:38:30 +0100

    Gestion de la connexion de l'utilisateur

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Ce qu'on a déjà fait :
 Installer node.js : https://nodejs.org/en/download/
 
 Lancer (à la racine du projet) : npm install -y
-Puis lancer : npm i --save express nodemon dotenv mongoose body-parser bcrypt jsonwebtoken
+Puis lancer : npm i --save express nodemon dotenv mongoose body-parser bcrypt jsonwebtoken cookie-parser
 
 Pour lancer le serveur : npm start
 
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const memberRoutes = require('./routes/member.routes')
 const patternRoutes = require('./routes/pattern.routes')
 const commentRoutes = require('./routes/comment.routes')
 require('dotenv').config({path: './config/.env'});
 require("./config/db")
+const {checkMember, requireAuth} = require('./middleware/auth.middleware')
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+
+// jwt
+app.get('*', checkMember)
+app.get("/jwtid", requireAuth, (req, res) => {
+    res.status(200).send(res.locals.member._id)
+})
 
 //routes
 app.use('/api/member', memberRoutes)
